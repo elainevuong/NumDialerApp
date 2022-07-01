d81d3460c1bbebdd31ab1e47b0a2e7684b9a487c
Elaine Vuong <elaine.yt.vuong@gmail.com>
2022-07-01 12:40:49 -0400

    Commited with Srdjans Comments

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,6 +3,8 @@ const axios = require('axios')
 const app = express()
 const PORT = 8000
 
+let numberIdx = 0;
+
 const phoneNumbers = [
   13018040009,
   19842068287,
@@ -15,210 +17,144 @@ const phoneNumbers = [
   19362072765,
 ]
 
+const phoneNumbersInProgress = {
+}
+
 app.use(express.json()); // middleware to access the request body
 
 app.get('/', (req, res) => {
   res.send('Hello!')
 })
 
-/*
-function processPhoneNumberTest(phoneNumber) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log('------------')
-      console.log(`processing phone number: ${phoneNumber}`)
-      resolve(phoneNumber)
-    }, 3000)
-  })
+// making 10 async functions running at most at the same time
+
+// first 3 calls at the same time, BUT you are not dialing 4th call once one of the first 3 ones resolves
+
+// 3 calls === 3 requests to the server  -> you get 3 responses back (with call ids). When you get response, 
+// async function is resolved 
+
+// you are subscribed to 3 webhooks for these 3 phone numbers that you dialed
+
+// only when you get status completed for one of these phone numbers you can dial another number
+
+function initialCalls(phoneNumbers) { // logic to determine which phone numbers are getting dispatched
+  let phoneCalls = [];
+
+  for (let idx = numberIdx; idx < 3; idx++) {
+    numberIdx = idx
+    phoneCalls.push(processPhoneNumber(phoneNumbers[idx], idx))
+  }
+  return phoneCalls;
+  // let copyNumbers = [...phoneNumbers]
+  // let currIndex = 0
+
+  // return new Promise ((resolve, reject) => {
+  //   let numberCompleted = 0;
+  //   let numberRunning = 0;
+
+  //   const callNextNumber = () => {
+  //     numberRunning += 1
+
+  //     let currentNumber = copyNumbers.shift()
+  //     processPhoneNumber(currentNumber, currIndex).then(() => {
+  //       numberRunning -= 1
+  //       numberCompleted += 1
+
+  //       console.log(`Number Requests Running: ${numberRunning}`)
+  //       console.log(`Number Requests Completed: ${numberCompleted}`)
+  //       console.log(`Remaining Requests: ${copyNumbers}`)
+
+  //       if (numberCompleted === phoneNumbers.length) {
+  //         resolve()
+  //       } else if (copyNumbers.length > 0) {
+  //         callNextNumber()
+  //         currIndex++
+  //       }
+  //     })
+  //   }
+
+  //   let index = 0
+  //   while (copyNumbers.length > 0 && index < maxCalls) {
+  //     callNextNumber()
+  //     index++
+  //     currIndex++
+  //   }
+  // })
 }
-*/
 
-function callNumbers(phoneNumbers, maxCalls = 3) {
-  let copyNumbers = [...phoneNumbers]
-  // console.log(copyNumbers)
-
-  return new Promise ((resolve, reject) => {
-    let numberCompleted = 0;
-    let numberRunning = 0;
-
-    const callNextNumber = () => {
-      numberRunning += 1
-
-      processPhoneNumber(copyNumbers.shift()).then(() => {
-        numberRunning -= 1
-        numberCompleted += 1
-
-        /*
-        console.log(`Numbers Completed: ${numberCompleted}`)
-        console.log(`Numbers of Calls In Progress: ${numberRunning}`)
-        console.log(`Remaining Numbers: ${copyNumbers}`)
-        console.log('-------------------')
-        */
-
-        if (numberCompleted === phoneNumbers.length) {
-          // console.log('Complete')
-          resolve()
-        } else if (copyNumbers.length > 0) {
-          // console.log('calling next number', )
-          callNextNumber()
-        }
-      })
+const processPhoneNumber = async (phoneNumber, index) => {  // logic to set up and send phone number to API
+  const webhookURL = `http://localhost:8000/webhooks/phone/${phoneNumber}`
+  const phoneNumObject = {
+    phone: String(phoneNumber),
+    webhookURL
+  }
+  const response = await axios.post(`http://localhost:4830/call`, phoneNumObject)
+  const id = response.data.id
+  if (!(id in phoneNumbersInProgress)) {
+    const phoneNumberProperties = {
+      phoneNumber: phoneNumber,
+      position: index,
+      status: 'idle'
     }
 
-    let index = 0
-    while (copyNumbers.length > 0 && index < maxCalls) {
-      callNextNumber()
-      index++
-    }
-  })
-}
+    phoneNumbersInProgress[id] = phoneNumberProperties;
+    return id;
+  }
+  // return new Promise((resolve, reject) => {
 
-const processPhoneNumber = async (phoneNumber) => {
-  return new Promise((resolve, reject) => {
-    console.log('within process Phone Number')
-    const webhookURL = `http://localhost:8000/webhooks/phone/${phoneNumber}`
-    console.log(phoneNumber)
-    const phoneNumObject = {
-      phone: String(phoneNumber),
-      webhookURL
-    }
-    const sendPostRequest = async () => {
-      try {
-        console.log(`Sending Post Request for Phone Number: ${phoneNumber}`)
-        const response = await axios.post(`http://localhost:4830/call`, phoneNumObject)
-        console.log(response.data)
-        resolve()
-      } catch (err) {
-        console.log(err)
-      }
-    }
-  
-    sendPostRequest()
 
-  })
+  //   const sendPostRequest = async () => {
+  //     try {
+  //       console.log(`Sending Post Request for Phone Number: ${phoneNumber}, Index Number: ${index}`)
+        
+       
+  //       console.log(`this is the response Id for Number: ${phoneNumber}: id: ${id}`)
+        
+        
+  //       resolve()
+  //     } catch (err) {
+  //       console.log(err)
+  //     }
+  //   }
+    
+  //   sendPostRequest()
+  // })
 };
 
-
-app.get('/startAllCalls', (req, res) => {
-  // Front End - clicks button - causes us to be directed to this route
-  // loop through the phone numbers and dispatch 3 at a time
-  // and I need to make requests to the API Server
-
-  // I need Axios(?)
-
-
-})
-
-app.post('/webhooks/phone/:phoneId', (req, res) => {
-  console.log(req.params)
+app.post('/webhooks/phone/:phoneId', (req, res) => {  //path that an API response is going to follow to send it's data
+  console.log('webhook response received')
+  
   let body = req.body
+
   console.log(body)
+
+
+  let id = req.body.id
+  let status = req.body.status
+
+  if (status === "completed" && numberIdx < phoneNumbers.length - 1) {
+    numberIdx++
+    let phoneNumber = phoneNumbers[numberIdx]
+
+    processPhoneNumber(phoneNumber, numberIdx)
+    // dial next number
+  }
+
+  phoneNumbersInProgress[id].status = status
+
+  console.log(phoneNumbersInProgress)
   res.status(200).send()
 })
 
-app.get('/startCall', (req, res) => {
-  callNumbers(phoneNumbers, 3).then(done => 
-    res.send('success!')
-  )
+// calling numbers and waiting for all of them to resolve
+app.get('/startCall', async (req, res) => { //path for when the user clicks the call button
+  let response = await Promise.all(initialCalls(phoneNumbers)); // [1270, 1271, 1272]
+  // callNumbers(phoneNumbers, 3).then(done => 
+  //   res.send('success!')
+  // )
+  res.send("Success")
 })
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`)
 })
-
-/*
-  - front-end -> user is going to click 'call'
-      -> sends a Get Request to the path on the back-end that is /startAllCalls
-    - when the user clicks call
-    - we want to dispatch the first 3 phone numbers to start calling
-    - use the 'callNumbers' functionality
-
-    BackEnd Server - needs to send a NEW REQUEST to the API Server
-      (?) -> we're within the /startAllCalls
-
-
-
-
-  POST Request
-  {
-    phoneNumber: 13018040009,
-    webHookURL: /callWebhook
-  }
-
-  Response from APIServer
-  {
-    id: 1283 
-  }
-
-  BackEnd Object Representation
-  {
-    id#1273: {
-      phoneNumber: 13018040009,
-      status: ringing,
-      position: 0
-    },
-    1274: {
-      phoneNumber:
-      status:
-      position: 1
-    }
-  }
-
-
-  Front-End/Back-End Communication
-  {
-    position: id
-  }
-
-  13018040009 
-  19842068287,
-  15512459377, 
-  19362072765, 
-  18582210308, 
-  13018040009,
-  19842068287,
-  15512459377,
-  19362072765,
-
-  If a Webhook comes through on the backend
-  affects id 1283
-  From the id get the position
-  Front-End -> now it knows which phone number based on position
-  to update
-    go into that position
-      change the status from ringing -> answered -> completed
-
-  
-  Front-End
-    - id -> phoneNumber(?) -> status
-  
-  if status = ringing, create a new object and add it to the array
-
-
-
-
-  [
-    {
-      phoneNumber: 777777777
-      status: rignging
-      id: 1273
-    }
- 
-  ]
-  
-
-array[0].id
-
-  When the Back-End gets a Webhook response from the API Server
-    - Back-End Updates the Back-End Object Representation
-
-
-  13018040009 idle
-
-  13018040009 ringing
-
-  13018040009 answered
-
-  13018040009 completed
-
-*/
